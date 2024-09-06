bcea6a8ac3a106d26812c33c4cc7ee48d39cb107
faresarar <faresarar455@hmail.com>
2024-09-06 21:48:48 +0100

    Implement the swiper widget design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 import '../widgets/custom_icon.dart';
 import '../widgets/sale_widget.dart';
@@ -27,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -62,7 +64,23 @@ class _HomeScreenState extends State<HomeScreen> {
                         suffixIcon: Icon(IconlyLight.search),
                       ),
                     ),
-                    const SaleWidget(),
+                    SizedBox(
+                      height: size.height * 0.25,
+                      child: Swiper(
+                        itemCount: 3,
+                        control: const SwiperControl(),
+                        itemBuilder: (context, index) {
+                          return const SaleWidget();
+                        },
+                        pagination: const SwiperPagination(
+                          alignment: Alignment.bottomCenter,
+                          builder: DotSwiperPaginationBuilder(
+                            activeColor: Colors.red,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               )
